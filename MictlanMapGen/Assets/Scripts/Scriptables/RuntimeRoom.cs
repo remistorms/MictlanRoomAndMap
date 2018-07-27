f5cbd2e6b810b95a0d847f4d120db172c1dbc095
REM_ <ultimate.spidey.18@gmail.com>
2018-07-26 19:53:36 -0500

    Updated Prefab and Created RuntimeRoom Scriptable Object

---
+++ MictlanMapGen/Assets/Scripts/Scriptables/RuntimeRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Runtime Room", menuName = "RemSO/Rooms")]
+public class RuntimeRoom : ScriptableObject {
+
+    public int roomID;
+    public int totalLayers;
+    public string roomName;
+    public string creatorName;
+    public LevelType levelType;
+    public RoomInPath roomInPath;
+    public RoomType roomType;
+    public Vector2Int roomSize;
+    public Vector2Int roomPosition;
+    public Tilemap collisionLayer;
+    public Tilemap triggerLayer;
+    public List<Tilemap> layerTilemaps;
+    public Dictionary<Vector2Int, TileBase> tilesPosition;
+    public List<Dictionary<Vector2Int, TileBase>> roomTilemaps;
+    public TileBase startingBase;
+}
